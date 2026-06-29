e0e0131d362b4379171afe67150a6d2b32895ba9
swadesh-231 <swadeshchatterjee512@gmail.com>
2026-06-29 12:58:33 +0530

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import { globalErrorHandler } from "./middlewares/globalErrorHandler.js";
 
 const app = express();
 app.use(express.json());
+
+app.use(globalErrorHandler);
 
 export default app;
